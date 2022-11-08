72bc4a671bffe4ff0b2a03d33edf1d123c85472d
Juan Vallejo <juanpavj1954@gmail.com>
2022-11-07 18:53:28 -0600

    agregando funcion para no refrescar la pagina al agregar elementos al input

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const btn = document.getElementById('btnPrincipal')
 
 console.log(btn);
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function(evento){
+    evento.preventDefault();
     const input = document.getElementById('inputPrincipal');
     console.log(input.value);
 })
