af554a7ea02dffa42e6123d234e7872b8078a22e
Juan Vallejo <juanpavj1954@gmail.com>
2022-11-07 23:11:46 -0600

    agregando funcion createTask (funcion flecha)

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const btn = document.getElementById('btnPrincipal')
 
-console.log(btn);
-
-
-btn.addEventListener('click', (evento) => {
+const createTask = (evento) => {
     evento.preventDefault();
     const input = document.getElementById('inputPrincipal');
     console.log(input.value);
-})
+}
+
+console.log(btn);
+
+
+btn.addEventListener('click', createTask);
