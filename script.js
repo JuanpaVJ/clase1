2a9abe0b61ef0a2229c8942caeefac1128789f05
Juan Vallejo <juanpavj1954@gmail.com>
2022-11-09 20:51:08 -0600

    agregando el input.value a la lista en la pagina

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ const createTask = (evento) => {
     evento.preventDefault();
     const input = document.getElementById('inputPrincipal');
     const value = input.value;
+    const task = document.getElementById('card_agregada');
     input.value = '';
-    console.log(value);
+    //?
+    const content = `<div>
+            <i class="far fa-check-square icon"></i>
+            <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content;
+    console.log(content);
 }
 
 console.log(btn);
