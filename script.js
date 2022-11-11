f1a57d6a1ea2badd82196199e8175d4243b47ef9
Juan Vallejo <juanpavj1954@gmail.com>
2022-11-10 23:01:28 -0600

    agregando comentarios complementarios de comillas inversas e innerHTML

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const createTask = (evento) => {
     const value = input.value;
     const task = document.getElementById('card_agregada');
     input.value = '';
-    //? para incorporar contenido html a java se utilizan las comillas inversas:
+    //? para incorporar contenido html a java se utilizan las comillas inversas y el valor que se quiera agregar desde javascript se coloca: ${valor}
     //! ALT 96 (tiene que ser en el teclado numerico) 
     const content = `<div>
             <i class="far fa-check-square icon"></i>
             <span class="task">${value}</span>
         </div>
         <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content; //!innerHTML devuelve o estblece la sintaxis html describiendo los descendientes del elemento
+    task.innerHTML = content; //!innerHTML: con esto se puede asignar codigo HTML <--> java
     console.log(content);
 }
 
