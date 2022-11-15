9c6df1dfa130893bbc36a1d01b23342746a5feb8
Juan Vallejo <juanpavj1954@gmail.com>
2022-11-14 23:17:20 -0600

    agregando elementos a la lista con check de completado, agregando variables dinamicas de html en java

--- script.js
+++ script.js
@@ -12,25 +12,37 @@ const createTask = (evento) => {
     //? para incorporar contenido html a java se utilizan las comillas inversas y el valor que se quiera agregar desde javascript se coloca: ${valor}
     //! ALT 96 (tiene que ser en el teclado numerico) 
 
-    console.log(check());
-    const content = `<div>
-            ${check}
-            <span class="task">${value}</span>
-        </div>
+
+    console.log(checkComplete());
+    const taskContent = document.createElement('div');
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement('span');
+    titleTask.classList.add('task');
+    titleTask.innerHTML = value;
+    taskContent.appendChild(titleTask);
+    const content = `
         <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content; //!innerHTML: con esto se puede asignar codigo HTML <--> java
+
+
+    //task.innerHTML = content; //!innerHTML: con esto se puede asignar codigo HTML <--> java
     //! un método de acceso rápido al contenido completo de un contenedor en HTML.
 
+    task.appendChild(taskContent);
     list.appendChild(task);
     //? Al elemento "list" quiero agregarle un hijo
 
     console.log(content);
 }
+
 console.log(btn);
 
+
 btn.addEventListener('click', createTask);
-const check = () => {
+
+const checkComplete = () => {
     const i = document.createElement('i');
-    i.classList.add('check-icon');
+    i.classList.add('far');
+    i.classList.add('icon');
+    i.classList.add('fa-check-square');
     return i;
 }
